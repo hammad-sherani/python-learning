1e513bfc8012aaf2b7e8a4612d791e9154ffe56c
Hammad Khan <hammad@blinkswag.com>
2025-09-22 22:56:41 +0500

    day two

---
+++ 02/variable.py
@@ -0,0 +1,21 @@
+# a = 1
+# b = 4
+
+# print(a + b)
+
+# fruits = ["apple", "banana", "cherry", 2, 3.5, True, None, "apple", "banana"]
+# for f in fruits:
+#     print(f)
+
+# number = 10
+
+# for i in range(101):print(i * number)
+
+
+
+number = int(input("Enter a number: "))
+
+for i in range(1, number + 1):
+    print(i, "x", number, "=", i * number)
+
+
